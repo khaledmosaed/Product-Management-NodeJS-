df306aeb2e6a8b43843b51e307a45064a5d7fa0f
khaled\k_mosaed <khaledmosaed2012@gmail.com>
2017-08-22 19:23:34 +0200

    order add order status

--- orders.js
+++ orders.js
@@ -16,125 +16,128 @@ var con = mysql.createConnection({
   host: "localhost",     //192.168.1.4
   user: "root",          // Server userName
   password: "",          // Server  Password
-  database:"purchases"   // DataBase Name
-  });
+  database: "purchases"   // DataBase Name
+});
 
 //** Check Connection **//
-  con.connect(function(err) {
-    if (err) throw err;
-    console.log("Database connected  for Order Table Now...!");
-  });
+con.connect(function (err) {
+  if (err) throw err;
+  console.log("Database connected  for Order Table Now...!");
+});
 
- // Insert Code Query-------------------------------?? 
- 
-var insertOrder = function (orderId, userId, totalPrice ,res){
+// Insert Code Query-------------------------------?? 
 
-    var sql = "INSERT INTO `order`(`orderId`, `userId`, `totalPrice`,`createAt`) "
-     +" VALUES ('"+orderId+"','"+userId+"' ,'"+totalPrice+"', NOW() )";
+var insertOrder = function (userId, totalPrice, status, res) {
 
-   console.log(sql);
-   con.query( sql, function (err, result) {
+  var sql = "INSERT INTO `orders`( `userId`, `totalPrice`,`createAt`,`status`) "
+    + " VALUES ('" + userId + "' ,'" + totalPrice + "', NOW(),'" + status + "' )";
+
+  console.log(sql);
+  con.query(sql, function (err, result) {
     if (err) throw err;
     console.log("1 record inserted");
-    var data = {insertId:result.insertId}
+    var data = { insertId: result.insertId }
     res.send(data);
-  }); 
+  });
 };
 
-  app.post('/insrtOrd', function (req, res) {
+app.post('/insrtOrd', function (req, res) {
 
-    var newOrder = req.body;
-    console.log(newOrder);
+  var newOrder = req.body;
+  console.log(newOrder);
 
-    var orderId = newOrder.orderId, 
-        userId = newOrder.userId,
-        totalPrice = newOrder.totalPrice ;
+  var userId = newOrder.userId,
+    totalPrice = newOrder.totalPrice,
+    status = newOrder.status;
 
-       insertOrder(orderId, userId, totalPrice ,res);
-  });
+  insertOrder(userId, totalPrice, status, res);
+});
 
 // Delete Code Query-------------------------------??
 
-var deleteOrder = function ( orderId , res){
-    var sql = " DELETE FROM `order` WHERE orderId ='"+orderId+"' ";
-    console.log(sql);
-    con.query(sql, function (err, result) {
-      if (err) throw err;
-      console.log("Number of records deleted: " + result);
-      res.send(result);
-    });
-  };
+var deleteOrder = function (orderId, res) {
+  var sql = " DELETE FROM `orders` WHERE orderId ='" + orderId + "' ";
+  console.log(sql);
+  con.query(sql, function (err, result) {
+    if (err) throw err;
+    console.log("Number of records deleted: " + result);
+    res.send(result);
+  });
+};
 
 app.delete('/deltOrder', function (req, res) {
-        //log data   
-        console.log(req.body);
-        var orderId =req.body.orderId ;
+  //log data   
+  console.log(req.body);
+  var orderId = req.body.orderId;
 
-        // delete record 
-        deleteOrder(orderId,res);
-    });
+  // delete record 
+  deleteOrder(orderId, res);
+});
 
 //Update Code Query --------------------------------- 
 
-var updateOrder = function ( orderId, userId, totalPrice ,res) {
-    
-  var sql = "UPDATE `order` SET userId='"+userId+"' , totalPrice = '"+totalPrice+"' "
-    +" WHERE orderId = '"+orderId+"'";
-      console.log(sql);
-        con.query(sql, function (err, result) {
-          if (err) throw err;
-          console.log(result + " records updated");
-          res.send(result);
-        });
-      };
+var updateOrder = function (orderId, userId, totalPrice, status, res) {
 
-      app.put('/updtOrder', function (req, res) {
-        //log data
+  var sql = "UPDATE `orders` SET userId='" + userId + "' , totalPrice = '" + totalPrice + "' ,status ='" + status + "' "
+    + " WHERE orderId = '" + orderId + "'";
+  console.log(sql);
+  con.query(sql, function (err, result) {
+    if (err) throw err;
+    console.log(result + " records updated");
+    res.send(result);
+  });
+};
 
-        var vare=req.body;
-        console.log(vare);
-      
-        var  orderId =vare.orderId ,
-             userId =vare.userId , 
-             price=vare.price,
-             totalPrice=vare.totalPrice  ,
-             createAt =vare.createAt ;
-        // update record 
-        updateOrder(orderId, userId, totalPrice ,res);
-    });
+app.put('/updtOrder', function (req, res) {
+  //log data
+
+  var vare = req.body;
+  console.log(vare);
+
+  var orderId = vare.orderId,
+    userId = vare.userId,
+    price = vare.price,
+    totalPrice = vare.totalPrice,
+    createAt = vare.createAt,
+    status = vare.status;
+  // update record 
+  updateOrder(orderId, userId, totalPrice, status, res);
+});
 
 // Search Code Query-------------------------------
 
-var searchOrder = function ( orderId, userId, totalPrice ,res) {
-    
-            var sql = " SELECT * FROM `order` WHERE "
-                        +" orderId LIKE '%"+orderId+"%' "
-                        +" AND userId LIKE '%"+userId+"%' "
-                        +" AND  totalPrice LIKE '%"+totalPrice+"%' ";
-            console.log (sql);
-             // el concole log
-            con.query(sql, function (err, result) {
-            if (err) throw err;
-            console.log("searched Value " + result);
-            res.send(result);
-            });  
-         };
-    
-         app.get('/serchOrder', function (req, res) {
-            //log data
-            var seavlue=req.query;
-            console.log(seavlue);
-    
-            var  orderId =seavlue.orderId ,
-                 userId =seavlue.userId , 
-                 totalPrice=seavlue.totalPrice;
-        
-            if (!orderId) {orderId = ""; }
-            
-            searchOrder(orderId, userId, totalPrice ,res);
-         });
+var searchOrder = function (orderId, userId, totalPrice, status, res) {
 
-  // Console output..
-  app.listen(3033 ,function(){
-       console.log("Example Listen to your port:3033");
+  var sql = " SELECT * FROM `orders` WHERE "
+    + " orderId LIKE '%" + orderId + "%' "
+    + " AND userId LIKE '%" + userId + "%' "
+    + " AND  totalPrice LIKE '%" + totalPrice + "%' "
+    + " AND status LIKE '%" + status + "%' ";
+  console.log(sql);
+  // el concole log
+  con.query(sql, function (err, result) {
+    if (err) throw err;
+    console.log("searched Value " + result);
+    res.send(result);
   });
+};
+
+app.get('/serchOrder', function (req, res) {
+  //log data
+  var seavlue = req.query;
+  console.log('serchOrder',seavlue);
+
+  var orderId = seavlue.orderId,
+    userId = seavlue.userId,
+    totalPrice = seavlue.totalPrice,
+    status = seavlue.status;
+
+  if (!orderId) { orderId = ""; }
+
+  searchOrder(orderId, userId, totalPrice, status, res);
+});
+
+// Console output..
+app.listen(3033, function () {
+  console.log("Example Listen to your port:3033");
+});
