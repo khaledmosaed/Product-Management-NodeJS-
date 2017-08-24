f25af9e9d5d754c376f165ec11c085fec313dc19
khaled\k_mosaed <khaledmosaed2012@gmail.com>
2017-08-24 19:15:39 +0200

    spreate file connection and use routin

--- product.js
+++ product.js
@@ -1,31 +1,50 @@
 /**
  * Created by k_mosaed on 8/16/2017
  */
-const express = require('express')
-const app = express()
-var bodyParser = require('body-parser');
-var multer = require('multer'); // v1.0.5
-var upload = multer(); // for parsing multipart/form-data
-var mysql = require('mysql');  // Module for connection
+var express = require('express');
+var router = express.Router();
+var con = require('./mysql.connection');  // Module for connection
 
-app.use(bodyParser.json()); // for parsing application/json
-app.use(bodyParser.urlencoded({ extended: true })); // for parsing application/x-www-form-urlencoded
 
-//* connection code 
-var con = mysql.createConnection({
-  host: "localhost",     //192.168.1.4
-  user: "root",          // Server userName
-  password: "",          // Server  Password
-  database:"purchases"   // DataBase Name
-});
+function getById(id) {
+    
+      //  create promise object 
+      var promise =  new Promise(function (resolve, reject) {
+        
+    
+        //  make query in database
+        var sql = "SELECT * FROM `products` WHERE  id = '" + id + "' ";
+        con.query(sql, function (err, result) {
+          
+          if (err) {
+            console.log('promis will finish next line with reject');    
+            reject(err)
+          }
+    
+          console.log("" , result);
+    
+          // resolve the promise
+          console.log('promis will finish next line with resolve'); 
+          var data = null ; 
+          if (result.length > 0)        
+            {
+              data = result[0];
+            }
+    
+          if(data){
+             resolve(data);
+          }else{
+            reject({err:"user not found"});
+          }
+        });
+      });
+    
+      console.log('return promise object');
+      return promise ; 
+};
+  
 
- //** Check Connection **//
-con.connect(function(err) {
-  if (err) throw err;
-  console.log("Database connected  for PRODUCT Table Now...!");
-});
-
-// Insert Code Query-------------------------------??
+// Insert Code Query-------------------------------
 
 var insertProduct = function(name , price, img , sku , description , notes, res){
     
@@ -40,24 +59,7 @@ var insertProduct = function(name , price, img , sku , description , notes, res)
     });
 };
 
-app.post('/insrtProd', function (req, res) {
-    // get data form http request
-    var newProduct  = req.body ; 
-    //log data
-    console.log(newProduct);
-
-    // assgin data to local variables
-    var name =newProduct.name , 
-        price=newProduct.price,
-        img=newProduct.img  ,
-        sku =newProduct.sku ,
-        description =newProduct.description ,
-        notes=newProduct.notes  ;
-    // insert record 
-    insertProduct(name , price, img , sku , description , notes,res)
-});
-
-// Delete Code Query-------------------------------??
+// Delete Code Query-------------------------------
 
 var deleteProduct = function (id , res){
     var sql = "DELETE FROM products WHERE id = '"+id+"' ";
@@ -69,15 +71,8 @@ var deleteProduct = function (id , res){
     });
 
 };
-app.delete('/deltProd', function (req, res) {
-        //log data
-        console.log(req.body);
-        var id =req.body.id ;
-        // delete record 
-        deleteProduct(id,res);
-    });
 
-//Update Code Query --------------------------------- 
+// Update Code Query--------------------------------
 
 var updateProduct = function (id ,name , price, img , sku , description , notes,res) {
     var sql = "UPDATE products SET name = '"+name+"' , price ='"+price+"', img = '"+img+"' "
@@ -88,29 +83,11 @@ var updateProduct = function (id ,name , price, img , sku , description , notes,
           console.log(result + " records updated");
           res.send(result);
         });
-      };
-
-      app.put('/updtProd', function (req, res) {
-        //log data
-
-        var vare=req.body;
-        console.log(vare);
-
-        var  id =vare.id ,
-             name =vare.name , 
-             price=vare.price,
-             img=vare.img  ,
-             sku =vare.sku ,
-             description =vare.description ,
-             notes=vare.notes  ;
-        // update record 
-        updateProduct(id , name , price, img , sku , description , notes,res);
-    });
-
+};
 
 // Search Code Query-------------------------------
 
-   var serchProduct = function (id ,name , img , sku ,price , description , notes,res) {
+ var serchProduct = function (id ,name , img , sku ,price , description , notes,res) {
 
            var  sql= "SELECT * FROM products WHERE id LIKE '%"+id+"%' "
                     +" AND name LIKE '%"+name+"%' "
@@ -128,27 +105,90 @@ var updateProduct = function (id ,name , price, img , sku , description , notes,
         console.log("searched Value " + result);
         res.send(result);
         });  
-     };
+ };
 
-     app.get('/serchProd', function (req, res) {
-        //log data
-        var seavlue=req.query;
-        console.log(seavlue);
 
-        var   id  =seavlue.id,
-              name =seavlue.name , 
-              price =seavlue.price,
-              img=seavlue.img  ,
-              sku =seavlue.sku ,
-              description =seavlue.description ,
-              notes=seavlue.notes  ;
-    
-        if (!id) { id = "" ;}
-        
-        serchProduct(id ,name , img , sku ,price , description , notes,res);
-     });
+/*
+ * GET userlist.
+ */
 
- // Console output..
-app.listen(3033, function () {
-    console.log('Example app listening on port 3033!')
+router.post('/insrtProd', function (req, res) {
+  // get data form http request
+  var newProduct  = req.body ; 
+  //log data
+  console.log(newProduct);
+
+  // assgin data to local variables
+  var name =newProduct.name , 
+      price=newProduct.price,
+      img=newProduct.img  ,
+      sku =newProduct.sku ,
+      description =newProduct.description ,
+      notes=newProduct.notes  ;
+  // insert record 
+  insertProduct(name , price, img , sku , description , notes,res)
 });
+
+router.delete('/deltProd', function (req, res) {
+  //log data
+  console.log(req.body);
+  var id =req.body.id ;
+  // delete record 
+  deleteProduct(id,res);
+});
+
+router.put('/updtProd', function (req, res) {
+  //log data
+
+  var vare=req.body;
+  console.log(vare);
+
+  var  id =vare.id ,
+       name =vare.name , 
+       price=vare.price,
+       img=vare.img  ,
+       sku =vare.sku ,
+       description =vare.description ,
+       notes=vare.notes  ;
+  // update record 
+  updateProduct(id , name , price, img , sku , description , notes,res);
+});
+
+router.get('/serchProd', function (req, res) {
+  //log data
+  var seavlue=req.query;
+  console.log(seavlue);
+
+  var   id  =seavlue.id,
+        name =seavlue.name , 
+        price =seavlue.price,
+        img=seavlue.img  ,
+        sku =seavlue.sku ,
+        description =seavlue.description ,
+        notes=seavlue.notes  ;
+
+  if (!id) { id = "" ;}
+  
+  serchProduct(id ,name , img , sku ,price , description , notes,res);
+});
+
+router.get('/productbyid', function (req, res) {
+    
+      var id = req.query.id ; 
+    
+      getById(id).then(function(data){
+        console.log('userbyid data',data);
+        res.send(data) ; 
+      })
+      .catch(function(error){
+        console.log('userbyid errr',error);
+        res.status(500).send(error) ; 
+      })
+     
+});
+
+
+
+module.exports = router;
+
+
