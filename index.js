903c384bea507af7d36b4ce274cdbf93f3b0de6e
khaled\k_mosaed <khaledmosaed2012@gmail.com>
2017-08-24 19:16:14 +0200

    file start up for project

---
+++ index.js
@@ -0,0 +1,26 @@
+const express = require('express')
+const app = express()
+var bodyParser = require('body-parser');
+var multer = require('multer'); // v1.0.5
+var upload = multer();   // for parsing multipart/form-data
+
+app.use(bodyParser.json()); // for parsing roulication/json
+app.use(bodyParser.urlencoded({ extended: true })); // for parsing application/x-www-form-urlencoded
+
+
+// my modules
+var userRouter = require('./user');
+var productRouter = require('./product');
+var orderRouter = require('./orders');
+var orderProductRouter = require('./orderproduct');
+
+
+app.use('/', userRouter);
+app.use('/', productRouter);
+app.use('/', orderRouter);
+app.use('/', orderProductRouter);
+  
+// Console output..
+app.listen(3033, function () {
+    console.log("Example Listen to your port:3033");
+  });
