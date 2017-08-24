38609331626cf1d15fadcc211f523880564e3922
khaled\k_mosaed <khaledmosaed2012@gmail.com>
2017-08-24 19:17:00 +0200

    file database connection with server

---
+++ mysql.connection.js
@@ -0,0 +1,21 @@
+/*   This file contains connection code 
+*    
+*
+**/
+
+var mysql = require('mysql');  // Module for connection
+
+var con = mysql.createConnection({
+    host: "localhost",      // 192.168.1.4
+    user: "root",           // Server userName
+    password: "",           // Server  Password
+    database: "purchases"   // DataBase Name
+  });
+  
+  //** Check Connection **//
+  con.connect(function (err) {
+    if (err) throw err;
+    console.log("Database connected Now...!");
+  });
+
+  module.exports = con ; 
